4d107cebe63fb72c880c84633c68025a8648c885
StrayChanutt <kauaschultzdasilva2@gmail.com>
2024-10-28 15:34:01 -0300

    Add files via upload

---
+++ AULAS FRONT END KAUÂ/aula 25.10/Funcoes.js
@@ -0,0 +1,17 @@
+function imprimirOlaMundo(){
+    console.log("Olá Mundo!")
+}
+
+function nome(nome, sobrenome){
+    console.log(`Ola ${nome} ${sobrenome}`)
+}
+
+nome("Cleber", "Abreu")
+nome("Maria", "Carmo")
+nome("Arthur", "Dias")
+
+function numeros(numero1, numero2){
+    const numerais = numero1 + numero2
+    return numerais
+}
+console.log(numero = numeros(7, 10))
